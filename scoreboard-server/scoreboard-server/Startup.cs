98151f72963cab165dea5080adda6b9eb199510b
Ignas Savickas <ignuxs34@gmail.com>
2017-09-23 19:44:23 +0300

    Init commit

---
+++ scoreboard-server/scoreboard-server/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace scoreboard_server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => c.SwaggerDoc("scoreboard_server", new Info()));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            const string swaggerUrl = "/swagger/scoreboard_server/swagger.json";
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.DocExpansion("none");
+                    c.SwaggerEndpoint(swaggerUrl, "Scoreboard Server");
+                });
+        }
+    }
+}
